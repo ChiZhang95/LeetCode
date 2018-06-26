1cdbdb5076d540a55be9d9786e0603867262646a
Chi <chizhang1@berkeley.edu>
2018-06-25 17:37:21 -0700

    added set matrix zeros

---
+++ setZeros.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        map<int,int> row;
+        map<int,int> col;
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j]==0){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(row[i]>0||col[j]>0) matrix[i][j] = 0;
+            }
+        }
+    }
+};
